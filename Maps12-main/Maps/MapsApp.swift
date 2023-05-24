c21031202515cd6b4b87cf9c1a5d84ecd5250642
SpaceHosting <132146013+SpaceHosting@users.noreply.github.com>
2023-05-24 17:40:25 +0200

    Add files via upload

---
+++ Maps12-main/Maps/MapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsApp.swift
+//  Maps
+//
+//  Created by hassoun on 23/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
